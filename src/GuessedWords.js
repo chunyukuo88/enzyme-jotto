248e3114f93eebd9fbebbbeb3e87a85534b31c25
chunyukuo88 <alexgochenour@gmail.com>
2020-08-01 23:45:26 -0400

    A few more passing tests as the component gets flushed out.

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 const GuessedWords = (props) => (
     <div data-test="component-guessed-words">
-        {props.guessedWords.length === 0 && Instructions()}
+        {props.guessedWords.length === 0
+            ? Instructions() : WordList(props)
+        }
     </div>
 );
 
@@ -12,6 +14,28 @@ const Instructions = () => (
         Guess a word!
     </div>
 );
+
+const WordList = (props) => {
+    const rowsOfGuessedWords = props.guessedWords.map((word, index) => (
+       <tr data-test="guessed-word" key={index}>
+           <td>{word.guessedWord}</td>
+           <td>{word.letterMatchCount}</td>
+       </tr>
+    ));
+    return (
+      <div data-test="guessed-words">
+        <h3>Guessed Words:</h3>
+        <table>
+            <thead>
+                <tr><th>Guess</th><th>Matching Letters</th></tr>
+            </thead>
+            <tbody>
+                { rowsOfGuessedWords }
+            </tbody>
+        </table>
+      </div>
+    );
+};
 
 GuessedWords.propTypes = {
     guessedWords: PropTypes.arrayOf(
--- src/__tests__/GuessedWords.test.js
+++ src/__tests__/GuessedWords.test.js
@@ -22,8 +22,8 @@ describe('GuessedWords.js ', ()=>{
     it('does not throw a warning with expected props.', ()=>{
         checkProps(GuessedWords, defaultProps);
     });
+    let wrapper;
     describe('WHEN: No words are guessed, ', ()=>{
-        let wrapper;
         beforeEach(()=>{
             wrapper = setup({ guessedWords: []});
         });
@@ -37,6 +37,25 @@ describe('GuessedWords.js ', ()=>{
         });
     });
     describe('WHEN: Words have been guessed, ', ()=>{
-
+        const guessedWords = [
+            { guessedWord: 'train', letterMatchCount: 3},
+            { guessedWord: 'agile', letterMatchCount: 1},
+            { guessedWord: 'party', letterMatchCount: 5},
+        ];
+        beforeEach(()=>{
+            wrapper = setup({ guessedWords });
+        });
+        it('THEN: It renders a component without error, ', ()=>{
+            const component = findByTestAttr(wrapper, 'component-guessed-words');
+            expect(component.length).toBe(1);
+        });
+        it('and it renders the "guessed words" section, ', ()=>{
+            const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+            expect(guessedWordsNode.text().length).not.toBe(0);
+        });
+        it('and it renders the correct number of guessed words.', ()=>{
+            const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word');
+            expect(guessedWordsNodes.length).toBe(guessedWords.length);
+        });
     });
 });
